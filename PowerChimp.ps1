0d032fb77c16efdad7cba7518cf3faa2553f1a60
Ben Vera <ben@soundcontrolrecords.com>
2017-06-13 20:06:23 -0500

    Ever needed to send a lot of emails without it looking like a email blast? here you go!

---
+++ PowerChimp.ps1
@@ -0,0 +1,35 @@
+function Send-BlogEmail {
+    param(
+       [Parameter(Mandatory=$false,Position=0,ValueFromPipeline=$true)] 
+       [string]$blogEmail
+    )
+
+    $EmailFrom = "EMAIL"
+    $EmailTo = $blogEmail 
+    $Subject = "SUBJECT" 
+    $Body = "BODY" 
+
+    $SMTPServer = "smtp.gmail.com" 
+    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
+    $SMTPClient.EnableSsl = $true 
+    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("EMAIL", "PASSWORD"); 
+    $SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+
+    #Getting Auth error? Visit https://www.google.com/settings/security/lesssecureapps 
+    #and Turn On Access for less secure apps
+
+}
+
+function Send-MassBlogFromList {
+
+    $PathToBlogList = "LIST OF EMAILS"
+    
+    $emails = Get-Content $PathToBlogList
+
+    foreach ($email in $emails) {
+
+        Send-BlogEmail $email
+
+    }
+
+}
